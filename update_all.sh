9d1d40e121d318c7070237a0a38cfa1c46e0c12c
Danilo Augusto <daniloaugusto.ita16@gmail.com>
2017-07-12 11:34:36 +0200

    updater

---
+++ update_all.sh
@@ -0,0 +1,11 @@
+# Homebrew
+brew update && brew upgrade && brew cleanup -s && brew prune && brew doctor
+# Apple Store
+mas upgrade
+# Ruby
+gem update
+# LaTeX
+tlmgr update --all
+# Python
+pip-review -a
+
